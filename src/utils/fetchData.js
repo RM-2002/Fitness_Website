e0285ec9536cbdfda80299c0b7eaf7787b21a15d
Roudrini Mukherjee <89720737+RM-2002@users.noreply.github.com>
2023-02-16 12:53:08 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ export const exerciseOptions = {
   
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': 'aece7bd9b9msh5261a7845c01e94p1e2154jsn5d5c51e6853f'
+    'X-RapidAPI-Key': '67e30833d5msh2aa57b3be8cb21bp12b8aajsnddd14d1033fe'
     
   }
 };
@@ -19,7 +19,7 @@ export const youtubeOptions = {
     method: 'GET',
     url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
     headers: {
-      'X-RapidAPI-Key': 'aece7bd9b9msh5261a7845c01e94p1e2154jsn5d5c51e6853f',
+      'X-RapidAPI-Key': '67e30833d5msh2aa57b3be8cb21bp12b8aajsnddd14d1033fe',
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
